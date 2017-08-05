f89d9e95fb691586f7a271c59c120997d62d1dcd
Hector <Hector@192.168.0.12>
2017-08-05 14:00:01 -0600

    Usuarios commit

---
+++ src/tarea3_201602598/Usuarios.java
@@ -0,0 +1,57 @@
+package tarea3_201602598;
+import java.util.Scanner;
+
+
+public class Usuarios {
+private String nombre[]=new String[5];
+    
+    
+public void IngresarUsuario(){
+    Scanner sc=new Scanner(System.in);
+    
+    for(int i=0;i<nombre.length;i++){
+        System.out.println("Ingrese un nombre: "+"("+(i+1)+")");
+        nombre[i]=sc.nextLine();
+    
+    }//for
+    System.out.println("-----------------");
+    this.ImprimirUsuario();
+}//IngresarUsuario
+
+public void ImprimirUsuario(){
+    for(int i=0;i<nombre.length;i++){
+         System.out.println("-----------------");
+        System.out.println((i+1)+". "+nombre[i]);
+    }
+}//imprimirUsuario
+    
+
+public void UsuarioPersonalizado(){
+    Scanner sc=new Scanner(System.in);
+    boolean per=false;
+    String nombreper;
+    System.out.println("Ingrese el nombre de usuario: ");
+    nombreper=sc.nextLine();
+    
+    for(int i=0;i<nombre.length;i++){
+        if(nombreper.equalsIgnoreCase(nombre[i])){
+            per=true;
+        }
+    }//for
+    if(per==true){
+        System.out.println();
+        System.out.println("Usuario: "+nombreper);
+        System.out.println("-----------------");
+    }
+    else{
+        
+        System.out.println();
+        System.out.println("No existe ningun usuario con ese nombre");
+        System.out.println("-----------------");
+    }
+        
+    
+}//userper
+
+
+}//class

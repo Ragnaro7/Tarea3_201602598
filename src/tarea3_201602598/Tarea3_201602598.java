e63a30d6f73aa069885acff141fef75bda347e74
Hector <>
2017-08-05 14:05:54 -0600

    main commit

---
+++ src/tarea3_201602598/Tarea3_201602598.java
@@ -0,0 +1,22 @@
+
+package tarea3_201602598;
+
+/**
+ *
+ * @author Hector
+ */
+public class Tarea3_201602598 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Usuarios prueba=new Usuarios();
+        prueba.IngresarUsuario();
+        
+        
+    }//main
+    
+}

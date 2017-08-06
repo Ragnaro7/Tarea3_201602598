ef9c77cd6c8a6db2df921b03f38e34153245fe6f
Hector <Hector@192.168.0.12>
2017-08-05 21:37:04 -0600

    commit3

--- src/tarea3_201602598/Tarea3_201602598.java
+++ src/tarea3_201602598/Tarea3_201602598.java
@@ -7,9 +7,7 @@ package tarea3_201602598;
  */
 public class Tarea3_201602598 {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
         // TODO code application logic here
     

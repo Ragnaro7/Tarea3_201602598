d016eccd78740369c8910f5f790ba92674d03b7f
Hector <Hector@192.168.0.12>
2017-08-05 20:18:21 -0600

    main

--- src/tarea3_201602598/Tarea3_201602598.java
+++ src/tarea3_201602598/Tarea3_201602598.java
@@ -12,9 +12,24 @@ public class Tarea3_201602598 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Usuarios prueba=new Usuarios();
-        prueba.IngresarUsuario();
+    
+       
+        /*System.out.println(); 
+                Scanner sc2=new Scanner(System.in);
+                System.out.println("Ingrese una palabra o frase");
+                String palin=sc2.nextLine();
+                Palindroma pal=new Palindroma();
+                
+        if(pal.espalindromo(palin)){
+             System.out.println();
+            System.out.println("Es una palabra/frase palindroma");
+        }
+        else
+        {
+             System.out.println();
+            System.out.println("No es palindroma");
+        }
+         System.out.println();*/
         
         
     }//main
